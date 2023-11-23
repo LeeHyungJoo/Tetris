cc6288d2e8d6adb56cb8fd55d0771761296a41c4
Yodle_94 <sj_artist@naver.com>
2023-11-24 08:25:52 +0900

    [Feat] Add Data File

---
+++ Tetris/TileData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris;
+class TileData
+{
+    public int Type { get; set; }
+    public int Height { get; set; }
+    public int Width { get; set; }
+    public int[,]? Coords { get; set; }
+    public string? ImgPath { get; set; }
+}
