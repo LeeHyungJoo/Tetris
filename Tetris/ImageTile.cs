e4f3b175d2ff4733029df700f138fd58b6033969
Yodle_94 <sj_artist@naver.com>
2023-11-24 11:55:49 +0900

    [Feat] Add ImageTile class

---
+++ Tetris/ImageTile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris;
+
+//Draw 객체를 상속 받는것이 맞는지 생각 중. 
+internal class ImageTile
+{
+}
