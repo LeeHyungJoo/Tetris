71394047a68e4e4aa95de570e3339fc4f781a4ee
Yodle_94 <sj_artist@naver.com>
2023-11-24 17:35:32 +0900

    [Feat] View - 비트 마스킹을 통해 특정 블럭 색칠

--- Tetris/TetrisForm.cs
+++ Tetris/TetrisForm.cs
@@ -1,3 +1,5 @@
+using System.Drawing;
+
 namespace Tetris
 {
     public partial class TetrisForm : Form
@@ -6,32 +8,54 @@ namespace Tetris
         private (int y, int x) _boardInitCoord;
         private int _size;
 
+        private Pen gridPen;
+        private Brush brush;
+
+        private int[] _masking;
+
         public TetrisForm()
         {
             InitializeComponent();
+            gridPen = new Pen(Color.DarkGray, 0.1f);
+            brush = new SolidBrush(Color.Azure);
+
             _board = new Board(24, 10)!;
             _size = 18;
             _boardInitCoord = (20, 20);
+
+            _masking = new int[10];
+            for(int i = 0; i < 10; i++)
+            {
+                _masking[i] = (1 << i);
+            }
         }
 
         private void DrawUI(Graphics g)
         {
-            Pen gridPen = new Pen(Color.DarkGray, 0.2f);
+            List<Rectangle> placedRectList = new List<Rectangle>();
             for (int y = 0; y < _board.Height; y++)
             {
                 for (int x = 0; x < _board.Width; x++)
                 {
-                    g.DrawRectangle(
-                        gridPen,
+                    var rect = new Rectangle(
                         _boardInitCoord.x + x * _size,
                         _boardInitCoord.y + y * _size,
-                        _size, _size
-                        );
-                    
+                        _size - 1, _size - 1);
+
+                    g.DrawRectangle(gridPen, rect);
+
+
+                    if ((_board.Placed[y] & _masking[x]) == _masking[x])
+                    {
+                        placedRectList.Add(rect);
+                    }
                 }
             }
-            gridPen.Dispose();
 
+            if (placedRectList.Count > 0)
+            {
+                g.FillRectangles(brush, placedRectList.ToArray());
+            }
         }
 
         protected override void OnPaint(PaintEventArgs e)
