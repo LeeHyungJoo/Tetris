72e4532f52b40f79dfc2e321766a33633e623b78
Yodle_94 <sj_artist@naver.com>
2023-11-24 15:52:55 +0900

    [Feat] Add Icon To Form

--- Tetris/TetrisForm.Designer.cs
+++ Tetris/TetrisForm.Designer.cs
@@ -28,10 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(TetrisForm));
+            SuspendLayout();
+            // 
+            // TetrisForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(425, 663);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "TetrisForm";
+            Text = "Tetris";
+            ResumeLayout(false);
         }
 
         #endregion
