2ba8af849dd2583ca1a74ccee8a5974546cfc49e
Yodle_94 <sj_artist@naver.com>
2023-11-24 11:47:32 +0900

    [Rename] MasterData

---
+++ Tetris/MasterData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris;
+
+class MasterData
+{
+    public static MasterData Instance => _instance.Value;
+
+    private static readonly Lazy<MasterData> _instance = new Lazy<MasterData>(()=> new MasterData());
+
+    private TileData? _tileData { get; set; }
+
+
+    public void LoadData()
+    {
+
+    }
+}
