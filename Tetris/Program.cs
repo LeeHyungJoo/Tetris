b0aba98b212a83739ebd1e816a469350dfd83697
Yodle_94 <sj_artist@naver.com>
2023-11-25 12:15:17 +0900

    [Feat] Load TileData

--- Tetris/Program.cs
+++ Tetris/Program.cs
@@ -8,8 +8,8 @@ namespace Tetris
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
+            TileFactory.Instance.MakeData();
+
             ApplicationConfiguration.Initialize();
             Application.Run(new TetrisForm());
         }
